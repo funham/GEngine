23adecf0096ce259c0eeed9d535d55c928e53140
funham <fun1ham@gmail.com>
2021-12-21 15:56:39 +0300

    added base for collision handling in Level

--- lib/GEngine/src/Game/Level.cpp
+++ lib/GEngine/src/Game/Level.cpp
@@ -4,9 +4,24 @@
 
 void Level::update(float dt)
 {
-    for (size_t i = 0; i < _gobjects.size(); i++)
+
+    for (size_t i = 0; i < _gobjects.size() - 1; i++)
     {
-        _gobjects[i]->update(dt);
+	for (size_t j = i; j < _gobjects.size(); j++)
+	{
+	    auto &a = _gobjects[i];
+	    auto &b = _gobjects[j];
+	
+	    Hit hit = a->collider->collides(b->collider);
+	    if (hit.is_valid)
+	    {
+		hit.solve(a->dynamics, b->dynamics);
+	    }
+	}
+    }
+    for (auto &gobj : _gobjects)
+    {
+        gobj->update(dt);
     }
 }
 
